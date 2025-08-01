836f4bf18f74535356493f9214fa517e3259fed7
mika1407 <koneautomaatio@hotmail.com>
2025-08-01 21:50:00 +0300

    expand added

--- src/pages/posts/post-list.tsx
+++ src/pages/posts/post-list.tsx
@@ -1,15 +1,25 @@
-import { Datagrid, List, ReferenceField, TextField, FunctionField } from 'react-admin';
+import { Datagrid, List, ReferenceField, TextField, FunctionField, useRecordContext } from 'react-admin';
+
+const PostPanel = () => {
+    const record = useRecordContext();
+    return <div>{record?.body}</div>
+}
 
 const PostList = () => (
     <List>
-        <Datagrid>
+        <Datagrid expand={<PostPanel />}
+        sx={{
+            '.RaDatagrid-headerCell': {
+                padding: '16px'
+            }
+        }}>
         {/* <DataTable>
             <DataTable.Col source="userId">
             </DataTable.Col> */}
             {/* <DataTable.Col source="id" />
             <DataTable.Col source="title" />
             <DataTable.Col source="body" /> */}
-
+        
             <TextField source="id" />
             <TextField  source="title" label="Post Title"/>
             <FunctionField label="Excerpt" render={(record) => `${record.body.substring(0, 50)}...`} />
