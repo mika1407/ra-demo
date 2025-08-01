e59a93492da78a43b0d7cddce76d89f7fec196b5
mika1407 <koneautomaatio@hotmail.com>
2025-08-01 22:18:49 +0300

    Users added

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,11 @@ import { Admin, Resource, EditGuesser, ShowGuesser } from 'react-admin';
 import { Layout } from "./Layout";
 import { dataProvider } from './dataProvider';
 import PostList from './pages/posts/post-list';
+import UserList from './pages/users/user-list';
 
 export const App = () => (
     <Admin layout={Layout} dataProvider={dataProvider}>
         <Resource name="posts" list={PostList} />
+        <Resource name="users" list={UserList} />
     </Admin>
 );
