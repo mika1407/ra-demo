ba53116815aab90381cd9eaf4777de126ab92337
mika1407 <koneautomaatio@hotmail.com>
2025-08-01 05:51:09 +0300

    added layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import { Admin, Resource, ListGuesser, EditGuesser, ShowGuesser } from 'react-admin';
+import { Layout } from "./Layout";
 import { dataProvider } from './dataProvider';
 
 export const App = () => (
-    <Admin dataProvider={dataProvider}>
+    <Admin layout={Layout} dataProvider={dataProvider}>
 
     </Admin>
 );
