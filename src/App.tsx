8e3babd84beb3dd2a45e1fedbf88907d6500a6e5
mika1407 <koneautomaatio@hotmail.com>
2025-08-04 05:49:20 +0300

    UserShow added

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
-import { Admin, Resource, EditGuesser, ShowGuesser } from 'react-admin';
+import { Admin, Resource, EditGuesser } from 'react-admin';
 import { Layout } from "./Layout";
 import { dataProvider } from './dataProvider';
 import PostList from './pages/posts/post-list';
+import PostShow from './pages/posts/post-show';
 import UserList from './pages/users/user-list';
+import UserShow from './pages/users/user-show';
 
 export const App = () => (
     <Admin layout={Layout} dataProvider={dataProvider}>
-        <Resource name="posts" list={PostList} />
-        <Resource name="users" list={UserList} />
+        <Resource name="posts" list={PostList} show={PostShow} />
+        <Resource name="users" list={UserList} show={UserShow}/>
     </Admin>
 );
