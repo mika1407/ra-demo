4209c8635a181f873fa4ac0f87a19624f5fc4fbb
mika1407 <koneautomaatio@hotmail.com>
2025-08-06 02:13:55 +0300

    homepage added

--- src/App.tsx
+++ src/App.tsx
@@ -7,10 +7,13 @@ import PostEdit from './pages/posts/post-edit';
 import PostCreate from './pages/posts/post-create';
 import UserList from './pages/users/user-list';
 import UserShow from './pages/users/user-show';
+import ArticleIcon from '@mui/icons-material/Article';
+import PersonIcon from '@mui/icons-material/Person';
+import { HomePage } from './pages/homepage';
 
 export const App = () => (
-    <Admin layout={Layout} dataProvider={dataProvider}>
-        <Resource name="posts" list={PostList} show={PostShow} edit={PostEdit} create={PostCreate}/>
-        <Resource name="users" list={UserList} show={UserShow}/>
+    <Admin layout={Layout} dataProvider={dataProvider} dashboard={HomePage}>
+        <Resource icon={ArticleIcon} name="posts" list={PostList} show={PostShow} edit={PostEdit} create={PostCreate}/>
+        <Resource icon={PersonIcon} name="users" list={UserList} show={UserShow}/>
     </Admin>
 );
---
+++ src/pages/homepage.tsx
@@ -0,0 +1,20 @@
+import { Card, CardContent, Typography, Box } from '@mui/material';
+
+export const HomePage = () => {
+    return (
+        <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh', }}>
+
+        <Card sx={{ maxWidth: 400, padding: 2, textAlign: 'center' }}>
+            <CardContent>
+                <Typography variant='h4' component='div' gutterBottom >
+                    Welcome to the Dashboard
+                </Typography>
+                <Typography variant='body1' color='text.secondary' >
+                    Manage your post and user effortlessly with the tools provided.
+                    Navigate through the menu to get started.
+                </Typography>
+            </CardContent>
+        </Card>
+        </Box>    
+    )
+}
